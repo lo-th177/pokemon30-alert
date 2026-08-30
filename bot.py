43caee4ab8fef2e521852a0f9e9af3cb03108026
lo-th177 <loic.thuriet@gmail.com>
2026-08-30 02:23:05 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print("Bot Pokémon 30e anniversaire démarré !")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = "1373516274"
+
+message = "🟢 TEST — Bot Pokémon 30e anniversaire connecté !"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print(response.text)
